07d28f4dde98e61b066bf86d9ac590fb3ce03c7d
Ryan Carpenter <dev.ryan1184@gmail.com>
2025-02-07 00:07:01 -0700

    Firebase Authentication Setup
    - Downloading Firebase Auth. Package
    - Updating Android SDK to v23

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart'; // Make sure this import works
 import 'ViewModels/LoginViewModel.dart'; // Adjust the path as per your project structure
 import 'Views/LoginView.dart'; // Adjust the path for your login page
 import 'Views/Splash_Screen.dart';
+
+// Firebase Imports
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   // Initialize Firebase
