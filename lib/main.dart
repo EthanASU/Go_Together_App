33c66080b534f00ce0d2b4772a8b28b291a09bef
Ryan Carpenter <dev.ryan1184@gmail.com>
2025-03-12 23:33:55 -0700

    Firebase Fix

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,11 @@ import 'Views/LoginView.dart'; // Adjust the path for your login page
 import 'Views/Splash_Screen.dart';
 import 'Views/profile_screen_setup.dart';
 import 'Views/Profile_Personal_SetUp.dart';
+// Firebase Imports
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
+
 // void main() {
 //   runApp(
 //     ChangeNotifierProvider(
@@ -46,8 +51,19 @@ import 'Views/Profile_Personal_SetUp.dart';
 //     return SplashScreen();
 //   }
 // }
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  // Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => LoginViewModel(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -62,7 +78,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       // Set ProfileScreen as the home widget instead of your login/create account flow
-      home: const ProfileSetUp(),  // Update this to match your class name
+      home: const ProfileSetUp(), // Update this to match your class name
     );
   }
 }
