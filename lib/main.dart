dbe38a050ad21afdd7f4c357c524600f02df940b
Raymond Zou <83727945+RaymondZ27@users.noreply.github.com>
2025-04-13 23:04:31 -0700

    Update main.dart
    added transportation options in match me section instead of add criteria button

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'Views/Splash_Screen.dart';
 import 'Views/profile_screen_setup.dart';
 import 'Views/Profile_Personal_SetUp.dart';
 import 'Widgets/My_Trips_Top_Navigation_Bar.dart';
+import '../ViewModels/Profile_Personal_View_Model.dart';
 // Firebase Imports
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -22,8 +23,10 @@ Future<void> main() async {
   runApp(
     MultiProvider(
       providers: [
+
         ChangeNotifierProvider(create: (_) => LoginViewModel()),
         ChangeNotifierProvider(create: (_) => ProfileViewModel()),
+        ChangeNotifierProvider(create: (context) => ProfilePersonalSetUpViewModel()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -70,3 +73,5 @@ class _SplashScreenWithDelayState extends State<SplashScreenWithDelay> {
     return SplashScreen();
   }
 }
+
+
